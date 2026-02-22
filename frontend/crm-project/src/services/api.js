1c912ea62ffc53dbab2e1d31349c78e5cd36264d
RashidVelery <rashivelery@gamil.com>
2026-02-22 15:30:46 +0530

    updated frontend

--- frontend/crm-project/src/services/api.js
+++ frontend/crm-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:2000/api",
+  baseURL: "https://crm-project-6olabackend.vercel.app/api",
   withCredentials: true, // 🔥 required for cookies
 });
 
